3dcb12dc686fc2eeb01ed83552357dfd9a262a3e
gunhawk008 <salman9710@gmail.com>
2020-10-12 23:39:31 +0530

    Cards Added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,48 @@
 
 @section('content')
 
-<h1> home page </h1>
-    
+<section>
+
+    <div class="row">
+        <div class="col-sm-6 ">
+            <div class="p-5  container">
+                    <div class="cards  d-inline-flex p-2  text-white ">
+                        <img src="frontend\img\target.png" alt=""></img>
+                        <h1 class="card-text">50+ Clients</h1>
+                    </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="p-5 container">
+                    <div class=" cards1  d-inline-flex p-2  text-white">
+                        <img src="frontend\img\wedding-rings.png" alt=""></img>
+                        <h1 class="card-text">Wedding Photoshoot</h1>
+                    </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-6 ">
+            <div class="p-5 container">
+                    <div class="cards  d-inline-flex p-2  text-white ">
+                        <img src="frontend\img\photography.png" alt=""></img>
+                        <h1 class="card-text">Professional Photoshoots</h1>
+                    </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="p-5 container">
+                    <div class=" cards1 d-inline-flex p-2  text-white">
+                        <img src="frontend\img\instagram.png" alt=""></img>
+                        <h1 class="card-text">50k+ Insta Followers</h1>
+                    </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+
 @endsection
