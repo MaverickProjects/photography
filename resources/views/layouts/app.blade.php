6acaced5f997ee103de3784ba464aef256c8326e
gunhawk008 <salman9710@gmail.com>
2020-10-14 00:13:40 +0530

    my work added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,13 +11,13 @@
         <link rel="stylesheet" href="/frontend/css/bootstrap.min.css">
     </head>
     <body>
-        
+
         <nav class="navbar navbar-expand-lg navbar-light fixed-top">
             <a class="navbar-brand" href="#">Photography</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-          
+
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
@@ -38,6 +38,10 @@
 
 
             @yield('content')
+
+
+
+
 
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
