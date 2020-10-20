a04400ad969bb12f139721909ca6ddba21521115
Karan Sosa <karansosa1995@gmail.com>
2020-10-20 23:22:27 +0530

    portfolio page added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Oswald&family=Roboto:wght@300&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="/frontend/css/style.css">
+        <link rel="stylesheet" href="/frontend/css/mobile.css">
         <link rel="stylesheet" href="/frontend/css/bootstrap.min.css">
         <link rel = "stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <!-- Slick Slider -->
@@ -25,13 +26,13 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
-                  <a class="nav-link" href="#">Home</a>
+                  <a class="nav-link" href="/">Home</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">About</a>
+                  <a class="nav-link" href="/about">About</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Portfolio</a>
+                    <a class="nav-link" href="/portfolio">Portfolio</a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="#">Contact</a>
@@ -44,35 +45,32 @@
             @yield('content')
 
 
-<section>
-<div class="footer">
-<i class="fa fa-instagram" aria-hidden="true"></i>
-<i class="fa fa-twitter" aria-hidden="true"></i>
-<i class="fa fa-facebook" aria-hidden="true"></i>
-<i class="fa fa-linkedin" aria-hidden="true"></i>
-
-
-
-</div>
-</section>
+        <section>
+        <div class="footer">
+        <i class="fa fa-instagram" aria-hidden="true"></i>
+        <i class="fa fa-twitter" aria-hidden="true"></i>
+        <i class="fa fa-facebook" aria-hidden="true"></i>
+        <i class="fa fa-linkedin" aria-hidden="true"></i>
+        </div>
+        </section>
 
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" ></script>
     <script src="/frontend/js/bootstrap.min.js"> </script>
     <script src="/frontend/js/slick.min.js"></script>
-<script>
-  $(document).ready(function(){
-    $('.slickSlider').slick({
-        infinite: true,
-        dots:true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        slidesToShow: 1,
-        autoplaySpeed: 3000
+    <script>
+      $(document).ready(function(){
+        $('.slickSlider').slick({
+            infinite: true,
+            dots:true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            slidesToShow: 1,
+            autoplaySpeed: 3000
+        });
     });
-});
-</script>
+    </script>
     </body>
 </html>
